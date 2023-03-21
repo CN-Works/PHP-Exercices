894aa8a1ed631f176522360f1a9df512e524717d
ChernyyOrel <chernyyorel.all@gmail.com>
2023-03-21 14:19:20 +0100

    Format adaptation for values

--- 5 - francs to euro converter/index.php
+++ 5 - francs to euro converter/index.php
@@ -7,5 +7,8 @@ $input = 100;
 // Converting Francs into Euro
 $result = $input * $conversion_ratio;
 
+// Formating for print
+$input = number_format($input,2,".","")
+$result = = number_format($result,2,".","")
 
 echo "Your ".$input." Francs worth ".$result."€ (Euro) !";
