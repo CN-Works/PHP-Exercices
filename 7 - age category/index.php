26c9f5e884c57c03daa2fd0adfadb69ce6d8cf87
ChernyyOrel <chernyyorel.all@gmail.com>
2023-03-21 15:53:10 +0100

    FIx : adding equal to condition

--- 7 - age category/index.php
+++ 7 - age category/index.php
@@ -7,9 +7,9 @@ $age = 10;
 
 if ($age > 6 and $age < 7) {
     echo "L’enfant qui a ".$age." ans appartient à la catégorie « Poussin »";
-} elseif ($age < 8 and $age > 9) {
+} elseif ($age <= 8 and $age >= 9) {
     echo "L’enfant qui a ".$age." ans appartient à la catégorie « Pupille »";
-} elseif ($age < 10 and $age > 11) {
+} elseif ($age <= 10 and $age >= 11) {
     echo "L’enfant qui a ".$age." ans appartient à la catégorie « Minime »";
 } elseif ($age >= 12) {
     echo "L’enfant qui a ".$age." ans appartient à la catégorie « Cadet »";
