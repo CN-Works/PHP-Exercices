86e79396b942306b5326ae9d075e610d0fd58084
ChernyyOrel <chernyyorel.all@gmail.com>
2023-03-21 11:22:32 +0100

    Palindrome logic function

--- 4 - palindrome detector/index.php
+++ 4 - palindrome detector/index.php
@@ -1,6 +1,17 @@
 <?php
 
 // Base Sentence in french
-$text = "Engage le jeu que je le gagne"
+$text = "Engage le jeu que je le gagne";
+$reversed_text='';
 
-// Reversing the text to check if it is a palindromic word
+// Reversing the text to check if it is a palindromic sentence
+for($i=strlen($text);$i>=0;$i--)
+{
+    $reversed_text.=$text[$i];
+}
+
+if ($text = $reversed_text) {
+    echo "The sentence < ".$text." > is a palindromic sentence !";
+} else {
+    echo "The sentence < ".$text." > is not a palindromic sentence !";
+}
