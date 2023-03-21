db702e1b09cf5793356155b296efe6285c66ee09
ChernyyOrel <chernyyorel.all@gmail.com>
2023-03-21 10:36:54 +0100

    Fix : variable format in string

--- 2 - counting words/index.php
+++ 2 - counting words/index.php
@@ -1,2 +1,2 @@
 $text = "Our php lesson begins today";
-echo "The sentence < $text > contain str_word_count($text); words !";
+echo "The sentence < $text > contain ".str_word_count($text)." words !";
