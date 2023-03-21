bb62f064a3c024d911876f939f87609b02e5888d
ChernyyOrel <chernyyorel.all@gmail.com>
2023-03-21 15:51:18 +0100

    Comparing age with category

---
+++ 7 - age category/index.php
@@ -0,0 +1,18 @@
+<?php
+
+// Default input
+$age = 10;
+
+// Main comparaison
+
+if ($age > 6 and $age < 7) {
+    echo "L’enfant qui a ".$age." ans appartient à la catégorie « Poussin »";
+} elseif ($age < 8 and $age > 9) {
+    echo "L’enfant qui a ".$age." ans appartient à la catégorie « Pupille »";
+} elseif ($age < 10 and $age > 11) {
+    echo "L’enfant qui a ".$age." ans appartient à la catégorie « Minime »";
+} elseif ($age >= 12) {
+    echo "L’enfant qui a ".$age." ans appartient à la catégorie « Cadet »";
+} else {
+    echo "L’enfant qui a ".$age." ans n'appartient à aucune catégorie.";
+}
