362c13313eea4107e19a17b0faa1fa335c542db0
ChernyyOrel <chernyyorel.all@gmail.com>
2023-03-21 14:40:14 +0100

    Fix : bad array manipulation

--- 6 - vat calculation/index.php
+++ 6 - vat calculation/index.php
@@ -2,3 +2,10 @@
 
 // Basket data | 1 = Price per item / 2 = Item amount
 $basket = array(9.99,5);
+$vat_ratio = 0.2;
+
+// Amount to pay
+$amount = $basket[1]*$basket[2];
+
+// Print
+echo "Total amount to pay : ".$amount;
