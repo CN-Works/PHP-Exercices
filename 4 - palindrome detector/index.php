2cc1e1a813313e3f74546eeb3e798fc097853d9d
ChernyyOrel <chernyyorel.all@gmail.com>
2023-03-21 11:58:29 +0100

    Trying to compare correctly

--- 4 - palindrome detector/index.php
+++ 4 - palindrome detector/index.php
@@ -8,7 +8,7 @@ $reversed_text='';
 
 // removing space in sentence
 str_replace(" ","",$text);
-echo "reversed : $text \n "
+echo "reversed : $text \n ";
 $modified_text = $text;
 $reversed_text = $modified_text;
 $cache_letters = "";
@@ -19,7 +19,7 @@ for($i=strlen($reversed_text);$i>=0;$i--)
     $cache_letters.=$reversed_text[$i];
 }
 
-if ($modified_text == $reversed_text) {
+if (strtolower($base_text) == $reversed_text) {
     echo "The sentence < ".$base_text." > is a palindromic sentence !";
 } else {
     echo "The sentence < ".$base_text." > is not a palindromic sentence !";
