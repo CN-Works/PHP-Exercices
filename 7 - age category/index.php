6777dba7da8d823fbc49e5bf35539e9e24d3bfaa
ChernyyOrel <chernyyorel.all@gmail.com>
2023-03-21 15:54:32 +0100

    Reversed some conditions

--- 7 - age category/index.php
+++ 7 - age category/index.php
@@ -1,15 +1,15 @@
 <?php
 
 // Default input
-$age = 10;
+$age = 11;
 
 // Main comparaison
 
-if ($age > 6 and $age < 7) {
+if ($age >= 6 and $age <= 7) {
     echo "L’enfant qui a ".$age." ans appartient à la catégorie « Poussin »";
-} elseif ($age <= 8 and $age >= 9) {
+} elseif ($age >= 8 and $age <= 9) {
     echo "L’enfant qui a ".$age." ans appartient à la catégorie « Pupille »";
-} elseif ($age <= 10 and $age >= 11) {
+} elseif ($age >= 10 and $age <= 11) {
     echo "L’enfant qui a ".$age." ans appartient à la catégorie « Minime »";
 } elseif ($age >= 12) {
     echo "L’enfant qui a ".$age." ans appartient à la catégorie « Cadet »";
