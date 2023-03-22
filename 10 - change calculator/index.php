e1c8fc6e94058bd1167661b90d69d8e24aa1ee2f
ChernyyOrel <chernyyorel.all@gmail.com>
2023-03-22 11:12:39 +0100

    Trying with modulo

---
+++ 10 - change calculator/index.php
@@ -0,0 +1,40 @@
+<?php
+
+// Scenario Data
+$rest_change = 48;
+echo "Amount : ".$rest_change."€";
+
+// Banknote 10
+if ($rest_change < 10) {
+    echo "\n\nBanknote 10 : 0";
+} else {
+    $banknote_ten = floor($rest_change/10);
+    $rest_change = $rest_change%10;
+    echo "\n\nBanknote 10 : ".$banknote_ten;
+}
+
+// Banknote 5
+if ($rest_change < 5) {
+    echo "\nBanknote 5 : 0";
+} else {
+    $banknote_five = floor($rest_change/5);
+    $rest_change = $rest_change % 5;
+    echo "\nBanknote 5 : ".$banknote_five;
+}
+
+// Coin 2
+if ($rest_change < 2) {
+    echo "\nCoin 2 : 0";
+} else {
+    $banknote_two = floor($rest_change/2);
+    $rest_change = $rest_change % 2;
+    echo "\nCoin 2 : ".$banknote_two;
+}
+
+// Coin 1
+if ($rest_change < 1) {
+    echo "\nCoin 1 : 0";
+} else {
+    $banknote_one = floor($rest_change/1);
+    echo "\nCoin 1 : ".$banknote_one;
+}
