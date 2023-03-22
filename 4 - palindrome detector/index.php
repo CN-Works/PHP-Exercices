139c8da5f9b3cb24134e519ca982207bbbe3c3d5
ChernyyOrel <chernyyorel.all@gmail.com>
2023-03-22 10:22:53 +0100

    Removing an old function

--- 4 - palindrome detector/index.php
+++ 4 - palindrome detector/index.php
@@ -11,18 +11,6 @@ $text_initial = strtolower($text);
 // Removing all the space
 $text_initial = str_replace(" ","",$text_initial);
 
-// String Reverse function
-
-function StringReverse($str)
-{
-    if (strlen($str) <= 1) return $str;
- 
-    $newstr = '';
-    $newstr .= substr($str,-1).StringReverse(substr($str,0,strlen($str)-1));
- 
-    return $newstr;
-}
-
 // Reversed string
 
 $text_reversed = strrev($text_initial);
