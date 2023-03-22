6147af0688eda633a846e92334cfdc7cca70af42
ChernyyOrel <chernyyorel.all@gmail.com>
2023-03-22 09:44:41 +0100

    Taxable : Trying a basic logic

--- 9 - impossibility/index.php
+++ 9 - impossibility/index.php
@@ -2,4 +2,18 @@
 
 // Personnal Data
 $age = 25;
-$sex = true;
+$sex = "m";
+$is_taxable = null;
+
+// Taxable check
+if ($sex == "m" and $age >= 20) {
+    $is_taxable = true;
+} elseif ($sex == "f" and ($age >= 18 and $age <= 35)) {
+    $is_taxable = true;
+}
+
+if ($is_taxable) {
+    echo "This person is taxable.";
+} else {
+    echo "This person isn't taxable.";
+}
