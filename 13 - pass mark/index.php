e99127d85943c6d168e3c004d76c903ae607525b
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2023-03-24 09:52:34 +0100

    Second attempt

--- 13 - pass mark/index.php
+++ 13 - pass mark/index.php
@@ -3,14 +3,25 @@
 // All students grades
 $grades = array(10,12,8,19,3,16,11,13,9);
 
-// Pass Mark function
-function GetPassMarkFromArray($list) {
+// Pass Mark & grades printing grades function
+function ShowPassMarkFromArray($list) {
     $total = 0;
-    for ($i = 0, $i <= count($list)) {
-        $total += $list[$i];
+    foreach ($list as $grade) {
+        $total += $grade;
     }
     $total = $total / count($list);
-    echo "Moyenne :".$total;
+    echo "Moyenne : ".number_format($total, 2,".","");
 }
 
-echo GetPassMarkFromArray($grades);
+function ShowGradesMessageFromGrades($grades) {
+    $text = "Students have obtained these grades :";
+    foreach ($grades as $number) {
+        $text = $text." ".$number;
+    }
+    echo $text;
+}
+
+// Printing Grades & Pass Mark
+
+
+echo ShowPassMarkFromArray($grades);
