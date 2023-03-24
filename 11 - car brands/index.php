e06fa880b272aa7816a53243a07147350bf754e1
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2023-03-24 08:52:07 +0100

    Fix : forgot a spacing

--- 11 - car brands/index.php
+++ 11 - car brands/index.php
@@ -4,7 +4,7 @@
 $cars = array("Mercedes","BMW","AUDI","BUGATTI");
 
 // Printing table lenght
-echo "There are ".count($cars)."different car brand in this table !";
+echo "There are ".count($cars)." different car brand in this table !";
 
 foreach ($cars as $brand) {
     echo "<br>- ".$brand;
