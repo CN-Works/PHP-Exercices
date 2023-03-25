1008c82e8d52b085c938335abf5d1aaa6efdb7c0
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2023-03-25 16:32:44 +0100

    Removing unused variable

--- 1 - couting chars/index.php
+++ 1 - couting chars/index.php
@@ -3,8 +3,5 @@
 // Sentence
 $text = "Our php lesson begins today";
 
-// Getting sentence's lengt
-$text_len = strlen($text);
-
 // Print
-echo "The sentence < $text > contain $text_len chars !";
+echo "The sentence < $text > contain ".strlen($text)." chars !";
