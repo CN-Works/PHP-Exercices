7b4b151df4faf00726a27e62b4844ee1c52c02cf
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2023-03-26 22:03:28 +0200

    Adding some comments

--- Part One/14_age_calculation/index.php
+++ Part One/14_age_calculation/index.php
@@ -1,15 +1,15 @@
 <?php
 
-// Default date
+// Default dates
 $originaldate = "17-01-1985";
 $todaysdate = date("d-m-Y");
 
-// Converting
+// Converting to date format
 $date = create_date($originaldate);
 $todaysdate = create_date($todaysdate);
 
 // Calc. Difference
-
 $result = date_diff($date,$todaysdate);
 
+// Printing result
 echo "You were born on ".$originaldate." !";
