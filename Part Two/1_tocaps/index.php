fe8dccc94c1c16a2edc5ba692a6ed5f1809d4e0f
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2023-03-27 10:59:06 +0200

    Adding comments

--- Part Two/1_tocaps/index.php
+++ Part Two/1_tocaps/index.php
@@ -3,14 +3,17 @@
 // Default input
 $text = "my default cool text";
 
+// Main function
 function toLowerRed($text) {
+    // just checking that input is a string
     if (gettype($text) == "string") {
         $text = strtoupper($text);
+        // the text text needs to be red
         return "<p style='color : red;'>".$text."</p>";
     } else {
         return "Your input is not a string !";
     }
 }
 
-
+// Printing result
 echo toLowerRed($text);
