c418b8215a19ba80f9bab92b873bf63bf933640c
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2023-03-27 11:06:53 +0200

    Default data and basic security

--- Part Two/2_capital_country/index.php
+++ Part Two/2_capital_country/index.php
@@ -1,1 +1,21 @@
 <?php
+
+// Default array
+$capitales = array(
+    "France" => "Paris",
+    "Germany" => "Berlin",
+    "USA" => "Washington",
+    "Italie" => "Rome",
+);
+
+// Main function using an array
+function showCountryCapitalesFromArray($capitales) {
+    if (gettype($text) == "array") {
+        return "This is an array with ".count($capitales)." countries !";
+    } else {
+        return "Your input is not an array !";
+    }
+};
+
+// Print result
+echo showCountryCapitalesFromArray("test");
