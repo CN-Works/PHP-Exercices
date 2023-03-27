a8db67b72363924f061abf1969c8970ea7f58110
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2023-03-27 12:04:50 +0200

    Adding an anchor for the link section

--- Part Two/4_capital_country_with_link/index.php
+++ Part Two/4_capital_country_with_link/index.php
@@ -13,13 +13,13 @@ $capitals = array(
 function showCountryCapitalesFromArray($capitals) {
     if (gettype($capitals) == "array") {
         // Opening
-        $result = "<table><tr><th>Country</th><th>Capital</th></tr></>";
-
+        $result = "<table><tr><th>Country</th><th>Capital</th><th>Link</th></tr></>";
+        $wikilink = "https://en.wikipedia.org/wiki/";
         // Adding every Key and Value to the table
         $temp_text = "";
         foreach ($capitals as $country => $capital) {
             // Upper case for country name
-            $temp_text = $temp_text."<tr><td>".strtoupper($country)."</td><td>".$capital."</td></tr>";
+            $temp_text = $temp_text."<tr><td>".strtoupper($country)."</td><td>".$capital."</td><td><a href='".$wikilink.$capital."' target='_blank'>here</a></td></tr>";
         }
 
         // Return result and closing table
