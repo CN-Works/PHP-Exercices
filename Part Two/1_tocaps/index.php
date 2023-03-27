e8fb283f633fc63a0304c1b99c43f3476d986ff9
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2023-03-27 10:55:08 +0200

    First attempt

--- Part Two/1_tocaps/index.php
+++ Part Two/1_tocaps/index.php
@@ -3,4 +3,14 @@
 // Default input
 $text = "my default lower case text";
 
-echo gettype($text);
+function toLowerRed($text) {
+    if (gettype($text) == "string") {
+        $text = strtoupper($text);
+        return "<p style='color : red;'>".$text."</p>";
+    } else {
+        return "Your input is not a string !";
+    }
+}
+
+
+echo toLowerRed($text);
