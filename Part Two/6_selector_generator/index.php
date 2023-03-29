bc4a2ed347a01da0e33d98354d2cbbcbae2b8e2a
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2023-03-29 16:05:16 +0200

    First attempt

--- Part Two/6_selector_generator/index.php
+++ Part Two/6_selector_generator/index.php
@@ -1,4 +1,34 @@
 <?php
 
 // Elements list
-$elements = array();
+$elements = array(
+    "Sausage",
+    "Cheese",
+    "Ham",
+    "Tomato",
+    "Chicken"
+);
+
+// Main function
+
+function showSelectorFromList($list) {
+    // Opening selection menu
+    $result = "<label for='food-select'>Choose an extra :</label> <select name='extra-selector'>";
+
+    // Adding a "none" option as default.
+    $result = $result." <option value='default'>None</option>";
+
+    // Item insert
+    foreach ($list as $item) {
+        $result = $result." <option value=".strtolower($item).">".$item."</option>";
+    }
+
+    // closing selection menu
+    $result = $result."</select>";
+
+    return $result;
+}
+
+// Print result
+
+showSelectorFromList($elements);
