7962ef5517e5836b524dd86da0aaeca3569711cd
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2023-03-31 04:46:40 +0200

    Importing table & dump

---
+++ Part Two/12_vardump/index.php
@@ -0,0 +1,13 @@
+<?php
+
+// A simple table with some data
+$classic_table = array(
+    true,
+    "texte",
+    10,
+    25.369,
+    array("valeur1","valeur2")
+);
+
+// Dumping the table
+echo var_dump($classic_table);
