be232979f63bad1bacfae41233c0c28f6f2dd784
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2023-03-31 04:55:15 +0200

    First Attempt

---
+++ Part Two/15_email_check/index.php
@@ -0,0 +1,16 @@
+<?php
+
+// default mail for testing
+$email = "john.doe@example.com";
+
+// checking function
+function checkingEmailFromInput($input) {
+    return filter_var($input, FILTER_VALIDATE_EMAIL);
+}
+
+// Printing result
+if checkingEmailFromInput($email) {
+    echo '"'.$email.'" is a valid email address !';
+} else {
+    echo '"'.$email.'" is not a valid email address !';
+}
