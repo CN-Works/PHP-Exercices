bb43567346c16e07372dc1389ede697b88cf8524
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2023-03-31 04:08:11 +0200

    Data & basic functions

---
+++ Part Two/10_account_creation/index.php
@@ -0,0 +1,57 @@
+<?php
+
+// Base data
+
+$available_sex = array(
+    "Male",
+    "Female",
+    // for people who don't want to give that information
+    "Unspecified",
+);
+
+$available_course = array(
+    "Web developer",
+    "Web design",
+    "Front-end developer",
+    "Back-end developer",
+);
+
+/* All UI Functions */
+
+// Generation function
+function showRadioSelectionFromList($list) {
+    // As always, checking inputs
+    if (gettype($list) != "array") {
+        return "Your input list is not an array, please try again.";
+    }
+    // base
+    $result = "<form> ";
+
+    foreach ($list as $item) {
+        $result = $result."<input type='radio' name='sex-selector'><label>".$item."</label><br>";
+    }
+
+    // closing form
+    $result = $result."</form>";
+
+    return $result;
+}
+
+// Selector function
+function showSelectorFromList($list) {
+    // Opening selection menu
+    $result = "<select name='extra-selector'>";
+
+    // Adding a "none" option as default.
+    $result = $result." <option value='default'>None</option>";
+
+    // Item insert
+    foreach ($list as $item) {
+        $result = $result." <option value=".strtolower($item).">".$item."</option>";
+    }
+
+    // closing selection menu
+    $result = $result."</select>";
+
+    return $result;
+}
