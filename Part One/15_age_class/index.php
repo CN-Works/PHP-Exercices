0760c7f1a5bdd64cd12fbe6e73468ab084385d7f
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2023-04-03 16:02:49 +0200

    Creating toString function

--- Part One/15_age_class/index.php
+++ Part One/15_age_class/index.php
@@ -5,14 +5,22 @@ class Person {
     // Personal data
     protected string $firstname ;
     protected string $lastname ;
+    
     // Date of birth
     protected string $dob;
 
     // Constructor
-
     public function __construct(string $firstname, string $lastname, string $dob) {
         $this->firstname = $firstname;
         $this->lastname = $lastname;
         $this->dob = $dob;
     }
+
+    public function __toString() {
+        // Age diff
+        $age = date_diff(date_create($this->dob),date_create(date("d-m-y")));
+        // returning a basic sentence
+        return $this->firstname." ".$this->lastname." is ".$age->format("%y year old.");
+    }
 }
+
