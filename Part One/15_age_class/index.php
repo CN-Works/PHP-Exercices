194fce0c8d387d18bff27cd769b3d73eea7d133a
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2023-04-03 15:36:48 +0200

    Construct with all properties

--- Part One/15_age_class/index.php
+++ Part One/15_age_class/index.php
@@ -5,7 +5,14 @@ class Person {
     // Personal data
     protected string $firstname ;
     protected string $lastname ;
-    protected $dob;
+    // Date of birth
+    protected string $dob;
 
     // Constructor
+
+    public function __construct(string $firstname, string $lastname, string $dob) {
+        $this->firstname = $firstname;
+        $this->lastname = $lastname;
+        $this->dob = $dob;
+    }
 }
