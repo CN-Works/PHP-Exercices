4d1773765e743fe5f9804add625232c72f746776
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2023-04-06 09:40:15 +0200

    Feat: Electric class

--- Part Two/14_cars_advanced/index.php
+++ Part Two/14_cars_advanced/index.php
@@ -1,9 +1,10 @@
 <?php
 
+// Base class
 class Car {
     // Basic information about the car
-    private string $brand;
-    private string $model;
+    protected string $brand;
+    protected string $model;
 
     // Arguments for class creation
     public function __construct(string $brand, string $model) {
@@ -20,9 +21,39 @@ class Car {
         return "This car is a ".$this->model." made by ".$this->brand." !";
     }
 
+    // Return basic information about the car
     public function getInformation() {
         return "This car is a ".$this->model." ".$this->brand.".";
     }
 }
 
+// Electric car class
+class ElectricCar extends Car {
+    protected int $autonomy;
+
+    // Arguments for class creation
+    public function __construct(string $brand, string $model, int $autonomy) {
+        // settings properties
+        $this->brand = $brand;
+        $this->model = $model;
+        $this->autonomy = $autonomy;
+
+        // Printing Creation data
+        echo "A new electric car is created : it's a ".$brand." ".$model." with ".$autonomy."kmh of autonomy.";
+    }
+
+    // Returns a presentation text (for fun)
+    public function __toString() {
+        return "This electric car is a ".$this->model." made by ".$this->brand." !";
+    }
+
+    // Return basic information about the car
+    public function getInformation() {
+        return "This electric car is a ".$this->model." ".$this->brand." with ".$autonomy."kmh of autonomy.";
+    }
+}
+
+// Testing Zone
 $cls63amg = new Car("Mercedes AMG","CLS 63");
+echo "<br> ----- <br>";
+$ami = new ElectricCar("Citroen","Ami",50);
