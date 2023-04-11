d938e2100e2559ae63dcfdce9d7f1821d698001e
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2023-04-11 09:59:52 +0200

    Feat: Getter for accounts data

--- Part Three/1_bankmanagement/index.php
+++ Part Three/1_bankmanagement/index.php
@@ -9,7 +9,7 @@ class BankClient {
     private string $city;
 
     // Acounts
-    private string $uuid = "mycoolbank_".rand(1, 999999) ;
+    private string $uuid = "MCB_".rand(1, 999999);
 
     private array $account_main = array(
         "currency" => "$",
@@ -30,5 +30,19 @@ class BankClient {
 
         // Printing Creation data
         echo $firstname." ".$lastname." just created an account at CoolBank's in ".$city." !";
+    }
+
+    // Get & Set methods
+    public function getAccountData(string $accounttype) {
+        // Main account
+        if ($accounttype == "main") {
+            return($this->account_main)
+        // Saving account
+        } elseif ($accounttype == "saving") {
+            return($this->account_saving)
+        // returning 0 if input isn't correct
+        } else {
+            return(0);
+        }
     }
 }
