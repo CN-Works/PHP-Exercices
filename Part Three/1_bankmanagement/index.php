e1b91cb26d45aa1a3e9fffdc164b93f5e789e8b8
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2023-04-11 09:04:33 +0200

    Feat: creating client class with basic information

---
+++ Part Three/1_bankmanagement/index.php
@@ -0,0 +1,14 @@
+<?php
+
+class BankClient {
+    // Personnal information
+    private string $firstname;
+    private string $lastname;
+    private string $dob;
+    
+    private string $city;
+
+    // Acounts
+    private float $account_main;
+    private float $account_saving;
+}
