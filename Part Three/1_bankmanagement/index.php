cef2920a5ee94145e8b27c0eb3d2af58676749e9
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2023-04-11 09:28:46 +0200

    Chore: Setting arguments & account structure

--- Part Three/1_bankmanagement/index.php
+++ Part Three/1_bankmanagement/index.php
@@ -9,6 +9,24 @@ class BankClient {
     private string $city;
 
     // Acounts
-    private float $account_main;
-    private float $account_saving;
+
+    private array $account_main = array(
+        "currency" => "$",
+        "amount" => 0.0,
+    );
+    private array $account_saving = array(
+        "currency" => "$",
+        "amount" => 0.0,
+    );
+
+    // Arguments
+    public function __construct(string $firstname, string $lastname, string $dob, string $city) {
+        // Personnal properties
+        $this->firstname = $firstname;
+        $this->lastname = $lastname;
+        $this->dob = $dob;
+        $this->city = $city;
+
+        // Printing Creation data
+    }
 }
