39e6e2655250b06cec8f83a89fc61d80e833e8d1
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2023-04-17 08:54:48 +0200

    Chore : Construct & Account getter

--- Part Three/1_bankmanagement/account.php
+++ Part Three/1_bankmanagement/account.php
@@ -2,6 +2,7 @@
 
 class Account {
     // Basic informations
+    public string $label;
     private string $currency;
     private float $amount;
 
@@ -9,11 +10,24 @@ class Account {
     private BankClient $client;
 
     // Arguments
-    public function __construct(BankClient $client, string $currency) {
+    public function __construct(BankClient $client, string $currency,string $label) {
+        // Setting up all args
         $this->client = $client;
         $this->currency = $currency;
+        $this->label = $label;
 
         // Just for testing
         $this->amount = 720.0;
+    }
+
+    public function getAccountData() : array {
+        $accountData = array(
+            "amount" => $amount,
+            "currency" => $currency,
+            "label" => $label,
+            "client" => $client,
+        );
+        // Returning all data
+        return $accountData;
     }
 }
