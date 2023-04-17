d47974555592931dc67166a8c7ec3803da577b2c
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2023-04-17 13:53:36 +0200

    Test : addings prints for a better code visibility

--- Part Three/1_bankmanagement/index.php
+++ Part Three/1_bankmanagement/index.php
@@ -13,24 +13,33 @@ function transferMoneyToOtherAccount(Account $account_one, Account $account_two,
         // Giving money to the second account
         $account_two->giveAccountMoney($amount);
         // Print
-        echo $account_one->getAccountData()["uuid"]." has transfered ".$amount.$account_one->getAccountData()["currency"]." to ".$account_two->getAccountData()["uuid"];
+        echo "<br>".$account_one->getAccountData()["uuid"]." has transfered ".$amount.$account_one->getAccountData()["currency"]." to ".$account_two->getAccountData()["uuid"];
     } else {
         echo "Invalid amount, negative value or sender's account has not enough money. ";
     }
 }
 
 // Testing
+
 echo "<br> -- Creating Clients -- <br>";
 $Ludwig = new BankClient("Ludwig","Meyer",date("d-m-y"),"Munich");
 $Felipe = new BankClient("Felipe","Salamanca",date("d-m-y"),"Los Cabos");
+
+// Creating accounts
 echo "<br> -- Creating Accounts -- <br>";
 $savings = new Account($Ludwig,"$","Saving account");
 $main = new Account($Felipe,"$","Main");
+
+// Account money
 echo "<br> -- Amount of money -- <br>";
 echo $savings->getAccountData()["amount"].$savings->getAccountData()["currency"];
+
+// Give & remove money
 echo "<br> ----- <br>";
 $savings->removeAccountMoney(50.0);
 echo "<br> ----- <br>";
 $savings->giveAccountMoney(50.0);
+
+// Money Transfer account to account
 echo "<br> -- Money transfer -- <br>";
 transferMoneyToOtherAccount($main,$savings,100);
