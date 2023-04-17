bf02b5c18600802ea7e55e20b405d6626a7fb468
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2023-04-17 09:02:06 +0200

    Fix : Adding requirements

--- Part Three/1_bankmanagement/index.php
+++ Part Three/1_bankmanagement/index.php
@@ -1,10 +1,12 @@
 <?php
 
+// Adding requirements
+require "account.php";
+require "client.php";
 
 // Testing
 
 $Ludwig = new BankClient("Ludwig","Meyer",date("d-m-y"),"Munich");
-$Alfred = new BankClient("Alfred","Bamer",date("d-m-y"),"London");
-
+//$Alfred = new BankClient("Alfred","Bamer",date("d-m-y"),"London");
 echo "<br> ----- <br>";
-var_dump();
+$savings = new Account();
