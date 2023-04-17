8e9fd4fe030e4f6df438045369f098f01e0d9dce
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2023-04-17 11:50:15 +0200

    Test : adding accounts

--- Part Three/1_bankmanagement/index.php
+++ Part Three/1_bankmanagement/index.php
@@ -5,14 +5,14 @@ require "account.php";
 require "client.php";
 
 // Testing
-
+echo "<br> -- Creating Clients -- <br>";
 $Ludwig = new BankClient("Ludwig","Meyer",date("d-m-y"),"Munich");
-//$Alfred = new BankClient("Alfred","Bamer",date("d-m-y"),"London");
-echo "<br> ----- <br>";
+$Felipe = new BankClient("Felipe","Salamanca",date("d-m-y"),"Los Cabos");
+echo "<br> -- Creating Accounts -- <br>";
 $savings = new Account($Ludwig,"$","Saving account");
-echo "<br> ----- <br>";
-$Ludwig->addAccount($savings);
-echo $savings->getAccountData()["amount"];
+$main = new Account($Felipe,"$","Main");
+echo "<br> -- Amount of money -- <br>";
+echo $savings->getAccountData()["amount"].$savings->getAccountData()["currency"];
 echo "<br> ----- <br>";
 $savings->removeAccountMoney(50.0);
 echo "<br> ----- <br>";
