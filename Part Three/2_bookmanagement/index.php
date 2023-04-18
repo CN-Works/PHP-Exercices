cbef240c8c21cd3d6650741c9d68f196332b7aef
ChernyyOrel <chernyyorel.all@gmail.com>
2023-04-18 11:39:50 +0200

    Removing class

--- Part Three/2_bookmanagement/index.php
+++ Part Three/2_bookmanagement/index.php
@@ -1,36 +1,8 @@
 <?php
 
-$registered_books = array();
-
-class Book {
-    // Information
-    private string $title;
-    private int $pages;
-    private float $price;
-    private string $publication;
-
-    // firsname x lastnames
-    private array $author;
-
-    // Arguments
-    public function __construct(string $title, string $author_firstname, string $author_lastname, int $pages, string $publication, float $price) {
-        $this->title = $title;
-        $this->author = array(
-            "firstname" => $author_firstname,
-            "lastname" => $author_lastname,
-        );
-        $this->pages = $pages;
-        $this->price = $price;
-        $this->publication = $publication;
-    }
-
-    // Presentation message
-    public function __toString() {
-        return "<strong>".$this->title."</strong> (".$this->publication.") : ".$this->pages." pages / ".$this->price."€";
-    }
-}
+require "Author.php";
+require "Book.php";
 
 // Testing
 
-$registered_books["Le fléau"] = new Book("Le fléau","Stephen","King",823,"1978",14);
-echo $registered_books["Le fléau"];
+$registered_books["Le fléau"] = new Book("Le fléau",823,"1978",14.45);
