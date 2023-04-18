d74fe205aa16666e5ed2ace5ded99400daeccac1
ChernyyOrel <chernyyorel.all@gmail.com>
2023-04-18 11:31:14 +0200

    Chore : author class

--- Part Three/2_bookmanagement/author.php
+++ Part Three/2_bookmanagement/author.php
@@ -3,4 +3,37 @@
 class Author {
     private string $firstname;
     private string $lastname;
+
+    public function __construct(string $firstname, string $lastname) {
+        // setting arguments
+        $this->firstname = $firstname;
+        $this->lastname = $lastname;
+    }
+
+    // Return name of the author
+    public function __toString() {
+        return $this->firstname." ".$this->lastname;
+    }
+
+    // Properties getters & setters
+
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+    }
+
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname($lastname)
+    {
+        $this->lastname = $lastname;
+    }
 }
