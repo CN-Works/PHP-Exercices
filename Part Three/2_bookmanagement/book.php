c5bbacee62d47dae69494ea988b0104e7b9199fa
ChernyyOrel <chernyyorel.all@gmail.com>
2023-04-18 11:40:17 +0200

    Refactor : book class & tostring

--- Part Three/2_bookmanagement/book.php
+++ Part Three/2_bookmanagement/book.php
@@ -1,1 +1,27 @@
 <?php
+
+class Book {
+    // Information
+    private string $title;
+    private int $pages;
+    private float $price;
+    private string $publicationDate;
+
+    // Author class
+    private Author $author;
+
+    public function __construct(string $title, int $pages, string $publicationDate, float $price) {
+        // setting arguments
+        $this->title = $title;
+        $this->pages = $pages;
+        $this->price = $price;
+
+        // Date
+        $this->publicationDate = $publicationDate;
+    }
+
+    // Presentation message
+    public function __toString() {
+        return "<strong>".$this->title."</strong> (".$this->publicationDate.")";
+    }
+}
