6a327e3bb20c978d72f25dd41ad374a657d1e64e
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2023-04-20 11:22:43 +0200

    Test : adding exemple

--- Part Three/2_bookmanagement/index.php
+++ Part Three/2_bookmanagement/index.php
@@ -7,8 +7,9 @@ require "Book.php";
 
 $StephenKing = new Author("Stephen","King");
 
-$fleau = new Book($StephenKing,"Le fléau",823,"1978",14.45);
+$fleau = new Book($StephenKing,"Le fléau",826,"1978",14.45);
 $fleau2 = new Book($StephenKing,"Le fléau2",823,"1978",14.45);
 $fleau3 = new Book($StephenKing,"Le fléau3",823,"1978",14.45);
+$fleau4 = new Book($StephenKing,"Le fléau4",823,"1978",14.45);
 
 echo $StephenKing->showAllBooks();
