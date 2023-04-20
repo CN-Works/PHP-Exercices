2272612d4b2e04b26a287a41c926aee77dfb5a02
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2023-04-20 08:33:11 +0200

    Fix : objectname

--- Part Three/2_bookmanagement/index.php
+++ Part Three/2_bookmanagement/index.php
@@ -9,6 +9,6 @@ $StephenKing = new Author("Stephen","King");
 
 $fleau = new Book($StephenKing,"Le fléau",823,"1978",14.45);
 $fleau2 = new Book($StephenKing,"Le fléau2",823,"1978",14.45);
-$fleau2 = new Book($StephenKing,"Le fléau3",823,"1978",14.45);
+$fleau3 = new Book($StephenKing,"Le fléau3",823,"1978",14.45);
 
 echo $StephenKing->showAllBooks();
