3cf1fa1695991dfeb82c27a8cf7217471fe7f087
ChernyyOrel <chernyyorel.all@gmail.com>
2023-04-21 13:58:32 +0200

    Removing producer class from person.php

--- Part Three/3_cinema/classes/Person.php
+++ Part Three/3_cinema/classes/Person.php
@@ -69,25 +69,3 @@ class Person {
         $this->dob = $dob;
     }
 }
-
-// Producer
-
-class Producer extends Person {
-    private array $movies = [];
-
-    
-    // Movies
-    public function getMovies(): array
-    {
-        return $this->movies;
-    }
-
-    public function setMovies($movies)
-    {
-        $this->movies = $movies;
-    }
-
-    public function addMovie (Movie $new_movie) {
-        $this->movies[] = $new_movie;
-    }
-}
