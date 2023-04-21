e5bc42ea4133a9d8f754afe9a0cd21b187b0cc53
ChernyyOrel <chernyyorel.all@gmail.com>
2023-04-21 11:00:58 +0200

    Chore : toString, getters & setters for Movie class
    Also removing old "actors" array

--- Part Three/3_cinema/classes/Movie.php
+++ Part Three/3_cinema/classes/Movie.php
@@ -13,20 +13,97 @@ class Movie {
     // Genre
     private Genre $genre;
 
-    // Actors
-    private array $actors = [];
-
     // Constructor
     public function __construct(string $title, string $releaseDate, int $duration, string $synopsys, Producer $producer, Genre $genre) {
         $this->title = $title;
         $this->releaseDate = new DateTime($releaseDate);
 
-        // Movie data
+        // Movie information
         $this->duration = $duration;
         $this->synopsys = $synopsys;
         $this->genre = $genre;
 
         // Producer
         $this->producer = $producer;
+    }
+
+    // returns title
+    public function __toString() {
+        return $this->title;
+    }
+
+    
+    // Getters & setter
+
+    public function getProducer()
+    {
+        return $this->producer;
+    }
+
+    public function setProducer($producer)
+    {
+        $this->producer = $producer;
+    }
+
+
+
+    public function getGenre()
+    {
+        return $this->genre;
+    }
+
+    public function setGenre($genre)
+    {
+        $this->genre = $genre;
+    }
+
+
+
+    public function getSynopsys()
+    {
+        return $this->synopsys;
+    }
+
+    public function setSynopsys($synopsys)
+    {
+        $this->synopsys = $synopsys;
+
+        return $this;
+    }
+
+
+
+    public function getDuration()
+    {
+        return $this->duration;
+    }
+
+    public function setDuration($duration)
+    {
+        $this->duration = $duration;
+    }
+
+
+
+    public function getReleaseDate()
+    {
+        return $this->releaseDate;
+    }
+
+    public function setReleaseDate($releaseDate)
+    {
+        $this->releaseDate = $releaseDate;
+    }
+
+
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function setTitle($title)
+    {
+        $this->title = $title;
     }
 }
