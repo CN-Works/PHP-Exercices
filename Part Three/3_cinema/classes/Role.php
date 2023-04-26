fc5ef9770cde868224e183525c7d448386dfb181
ChernyyOrel <chernyyorel.all@gmail.com>
2023-04-26 10:54:22 +0200

    Update Role.php

--- Part Three/3_cinema/classes/Role.php
+++ Part Three/3_cinema/classes/Role.php
@@ -51,7 +51,7 @@ class Role {
     // Generals
 
     public function showAllCharacterApparition() {
-        $text = "<h2>".$this->roleName." role</h2><br>";
+        $text = "<h2>".$this->roleName." apparitions</h2><br>";
 
         foreach($this->castings as $info) {
             $text .= "Interpreted by ".$info["actor"]." in ".$info["casting"]->getMovie()."<br>";
