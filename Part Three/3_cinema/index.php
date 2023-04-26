9a9f295169e07d3a7312c916c5108437005ede82
ChernyyOrel <chernyyorel.all@gmail.com>
2023-04-26 09:21:33 +0200

    Test: update

--- Part Three/3_cinema/index.php
+++ Part Three/3_cinema/index.php
@@ -5,6 +5,16 @@ spl_autoload_register(function ($class_name) {
     include "classes/" . $class_name . '.php';
 });
 
+// Producers
 $producer_one = new Producer("Daniel","Pelard","m","15-10-1990");
-
 echo $producer_one;
+echo "<br>----------<br>";
+echo "Born in ".date_format($producer_one->getDob(),"d M Y");
+
+// Genre
+$genreFiction = New Genre("Fiction");
+
+// Movies
+$movie1 = new Movie("Devils in space","20-05-2005",115,"Cool film with devils in space",$producer_one,$genreFiction);
+echo "<br>----------<br>";
+echo $genreFiction->showAllMovies();
