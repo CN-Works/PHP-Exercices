d0fecae485ad94e712645c9560c6ee3c1a032918
ChernyyOrel <chernyyorel.all@gmail.com>
2023-04-26 11:19:13 +0200

    Tests : update

--- Part Three/3_cinema/index.php
+++ Part Three/3_cinema/index.php
@@ -15,16 +15,24 @@ $genreFiction = New Genre("Fiction");
 
 // Actors
 $actor1 = new Actor("Marc","Johnson","m","28-06-2001");
+$actor2 = new Actor("Michel","Moustache","m","18-03-1995");
 
 // Roles
 $james_bond = new Role("James Bond");
+$batman = new Role("Batman");
 
 // Movies
 $movie1 = new Movie("Devils in space","20-05-2005",115,"Cool film with devils in space",$producer_one,$genreFiction);
+$movie2 = new Movie("Devils in space 2","20-05-2008",115,"Cool film with devils in space 2",$producer_one,$genreFiction);
 
 // Casting
 $casting1 = new Casting($movie1,$producer_one);
 $casting1->addingActorsAndRoles($actor1,$james_bond);
+$casting1->addingActorsAndRoles($actor1,$batman);
+$casting1->addingActorsAndRoles($actor2,$batman);
+// Second tests
+$casting2 = new Casting($movie2,$producer_one);
+$casting2->addingActorsAndRoles($actor2,$james_bond);
 
 ////////////////////////////////////////////
 
